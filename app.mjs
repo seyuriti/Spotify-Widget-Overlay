00a5c63c1c7a9d6d7950cc0f1730eda8f72a7529
seyuriti <aesioseyuri@gmail.com>
2024-10-19 22:38:15 -0500

    Commiting changing devices (error in app.mjs)

--- app.mjs
+++ app.mjs
@@ -1,16 +1,88 @@
 import express from 'express';
+import { request } from 'express';
 import config from './src/config.mjs';
 import { generateRandomString } from './src/utils/index.mjs';
 
 const app = express();
 
-app.get('/login', () => {
-  const state = generateRandomString(16);
+app.get('/validation', (req, res) => {
+  const error = req.query.error || null;
+
+  if (error === null){
+    res.status(200).send('Data stored successfully');
+  }
   
+  if (error === 'state_mismatch') {
+    res.status(400).send('State mismatch error');
+  }
+  else if (error === 'access_denied') {
+    res.status(403).send('Access denied error');
+  }
+  else {
+    res.status(400).send('Unknown error');
+  }
+})
+
+app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  const scope = config.scope;
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    new URLSearchParams({
+      response_type: 'code',
+      client_id: config.clientId,
+      scope,
+      redirect_uri: config.redirectUri,
+      state
+    })
+  );
 });
 
-app.post('/callback', (req, res) => {
+app.get('/callback', async (req, res) => {
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+  const error = req.query.error || null;
 
+  if (state === null) {
+    res.redirect('/validation?' +
+      new URLSearchParams({
+        error: 'state_mismatch'
+      })
+    );
+  }
+  else if (error !== null) {
+    res.redirect('/validation?' +
+      new URLSearchParams({
+        error: 'access_denied'
+      })
+    );
+  }
+  else {
+    let authOptions = {
+      form: {
+        code: code,
+        redirect_uri: config.redirectUri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + (new Buffer.from(config.clientId + ':' + config.clientSecret).toString('base64'))
+      },
+      json: true
+    };
+
+    await fetch('https://accounts.spotify.com/api/token', authOptions)
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then(data => {
+        res.status(200).send(data);
+      })
+      .catch(err => {
+        res.status(400).send(err);
+      });
+  }
 });
 
 app.listen(config.port, '0.0.0.0', () => {
--- src/config.mjs
+++ src/config.mjs
@@ -1,4 +1,5 @@
 import { config as loadEnv } from "dotenv";
+import e from "express";
 loadEnv()
 
 const port = process.env.PORT || 8900;
@@ -11,9 +12,21 @@ if (
   throw new Error("CLIENT_ID and CLIENT_SECRET must be provided");
 }
 
+if (process.env.SCOPE === undefined) {
+  throw new Error("SCOPE must be provided");
+}
+
 export default {
   port,
   redirectUri,
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
+  scope: process.env.SCOPE,
+  auth: {
+    access_token: null,
+    refresh_token: null,
+    expires_in: null,
+    token_type: null,
+    scope: null
+  }
 }
